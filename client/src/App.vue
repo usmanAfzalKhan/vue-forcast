5f835f77a6679f282ec8488114dd3fdafdcdcc48
Usman Khan <109322442+usmanAfzalKhan@users.noreply.github.com>
2024-03-09 13:16:16 -0500

    feat(client): add weather display component (#4)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <WeatherDisplay/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WeatherDisplay from './components/weather-display/WeatherDisplay.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WeatherDisplay
   }
 }
 </script>
---
+++ client/src/components/weather-display/WeatherDisplay.vue
@@ -0,0 +1,56 @@
+<template>
+    <h1>City</h1>
+    <p>Description</p>
+    <div class="weather-display">
+        <table class="weather-display-table">
+            <tr>
+                <th class="weather-display-table-header">Temperature</th>
+                <th class="weather-display-table-header">Feels Like</th>
+                <th class="weather-display-table-header">Maximum Temperature </th>
+                <th class="weather-display-table-header">Minimum Temperature </th>
+            </tr>
+            <tr>
+                <td>15</td>
+                <td>14</td>
+                <td>12</td>
+                <td>6</td>
+            </tr>
+        </table>
+        <table class="weather-display-table">
+            <tr>
+                <th class="weather-display-table-header">Pressure</th>
+                <th class="weather-display-table-header">Humility</th>
+                <th class="weather-display-table-header">Degrees </th>
+                <th class="weather-display-table-header">Wind Speed </th>
+            </tr>
+            <tr>
+                <td>60</td>
+                <td>75</td>
+                <td>250</td>
+                <td>75</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WeatherDisplay'
+}
+</script>
+
+<style scoped>
+    .weather-display {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+    }
+
+    .weather-display-table {
+        width: 100%;
+    }
+    
+    .weather-display-table-header{
+        width: 25%;
+    }
+</style>
