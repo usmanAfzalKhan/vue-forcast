e8f3765d35eaf30fcc2d6099b28d82b28ba15106
Syed Zaidi <26808641+zaidi-01@users.noreply.github.com>
2024-03-19 17:22:18 -0400

    feat: add Swagger UI (#9)
    * feat: add apispec and flask_apispec dependencies
    
    * feat: add swagger ui
    
    - Add shared module
    - Add swagger docs to shared module
    - Initialize swagger docs on server startup
    
    * docs: add weather API swagger documentation

--- server/server.py
+++ server/server.py
@@ -1,10 +1,55 @@
 from flask import Flask
-from api import weather_bp
 
-app = Flask(__name__)
+from api import weather_bp, register_weather_docs
+from constants import ENDPOINTS
+from shared import spec, initialize_docs
 
 
-app.register_blueprint(weather_bp)
+def create_app() -> Flask:
+    """
+    Create and configure the Flask application.
+
+    Returns:
+        Flask: The Flask application object.
+    """
+    app = Flask(__name__)
+    return app
+
+
+def register_blueprints(app: Flask):
+    """
+    Register the blueprints for the Flask application.
+
+    Args:
+        app (Flask): The Flask application object.
+    """
+    app.register_blueprint(weather_bp)
+    register_weather_docs()
+
+
+def register_docs(app: Flask):
+    """
+    Register the FlaskApiSpec instance for generating API documentation.
+
+    Args:
+        app (Flask): The Flask application object.
+    """
+    app.config.update(
+        {
+            "APISPEC_SPEC": spec,
+            "APISPEC_SWAGGER_URL": ENDPOINTS.DOCS + "/swagger.json",
+            "APISPEC_SWAGGER_UI_URL": ENDPOINTS.DOCS,
+        }
+    )
+    initialize_docs(app)
+
+
+# Create the Flask application
+app = create_app()
+# Register the swagger documentation
+register_docs(app)
+# Register the blueprints
+register_blueprints(app)
 
 
 if __name__ == "__main__":
---
+++ server/shared/__init__.py
@@ -0,0 +1,3 @@
+from shared.docs.docs import spec, initialize_docs, get_docs
+
+__all__ = ["spec", "initialize_docs", "get_docs"]
